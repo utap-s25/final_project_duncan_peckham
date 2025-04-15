84e1634112e6e7b5133138410f7e99058a3bbe0a
duncanp781 <hazard.dp@gmail.com>
2025-04-14 20:58:52 -0400

    Small todo list

--- app/src/main/java/edu/cs371m/wikirank/ViewModelDBHelper.kt
+++ app/src/main/java/edu/cs371m/wikirank/ViewModelDBHelper.kt
@@ -13,6 +13,14 @@ class ViewModelDBHelper {
     // If we want to listen for real time updates use this
     // .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
     // want to use this to listen to votes on current matchup
+
+
+    //todo db design:
+    // get a list of top 100 people (excluding some people), upload them to the db.
+    // Then design a function to query that, use that to send a wiki request to get more info.
+    // Have another collection for match-ups, which will have the two options and the winning option.
+    // (Will need to pull this whole collection when calculating elo scores locally, unless I want to do gradual calculations online)
+    // have another collection for a user's favorite articles
     private fun limitAndGet(query: Query,
                             resultListener: (List<Unit>)->Unit) {
         query
